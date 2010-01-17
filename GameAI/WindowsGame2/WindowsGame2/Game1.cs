cefc0ef15ef0e67e09cc78007684751e189b7258
Smith <Smiter4@gmail.com>
2010-01-17 16:21:47 +0300

    new file:   GameAI/WindowsGame2/WindowsGame2.sln  	new file:   GameAI/WindowsGame2/WindowsGame2.suo  	new file:   GameAI/WindowsGame2/WindowsGame2/Content/Content.contentproj  	new file:   GameAI/WindowsGame2/WindowsGame2/Content/ball.bmp  	new file:   GameAI/WindowsGame2/WindowsGame2/Content/obj/x86/Debug/Content.contentproj.FileListAbsolute.txt  	new file:   GameAI/WindowsGame2/WindowsGame2/Content/obj/x86/Debug/ContentPipeline.xml  	new file:   GameAI/WindowsGame2/WindowsGame2/Content/obj/x86/Debug/ResolveAssemblyReference.cache  	new file:   GameAI/WindowsGame2/WindowsGame2/Game.ico  	new file:   GameAI/WindowsGame2/WindowsGame2/Game1.cs  	new file:   GameAI/WindowsGame2/WindowsGame2/GameThumbnail.png  	new file:   GameAI/WindowsGame2/WindowsGame2/Program.cs  	new file:   GameAI/WindowsGame2/WindowsGame2/Properties/AssemblyInfo.cs  	new file:   GameAI/WindowsGame2/WindowsGame2/WindowsGame2.csproj  	new file:   GameAI/WindowsGame2/WindowsGame2/WindowsGame2.csproj.Debug.cachefile  	new file:   GameAI/WindowsGame2/WindowsGame2/WindowsGame2.csproj.user  	new file:   GameAI/WindowsGame2/WindowsGame2/bin/x86/Debug/Content/ball.xnb  	new file:   GameAI/WindowsGame2/WindowsGame2/bin/x86/Debug/WindowsGame2.exe  	new file:   GameAI/WindowsGame2/WindowsGame2/bin/x86/Debug/WindowsGame2.pdb  	new file:   GameAI/WindowsGame2/WindowsGame2/clsSprite.cs  	new file:   GameAI/WindowsGame2/WindowsGame2/obj/x86/Debug/Microsoft.Xna.Framework.RuntimeProfile.txt  	new file:   GameAI/WindowsGame2/WindowsGame2/obj/x86/Debug/ResolveAssemblyReference.cache  	new file:   GameAI/WindowsGame2/WindowsGame2/obj/x86/Debug/WindowsGame2.csproj.FileListAbsolute.txt  	new file:   GameAI/WindowsGame2/WindowsGame2/obj/x86/Debug/WindowsGame2.exe  	new file:   GameAI/WindowsGame2/WindowsGame2/obj/x86/Debug/WindowsGame2.pdb  	new file:   GameAI/WindowsGame2/WindowsGame2/obj/x86/Debug/cachefile-ef4e76bc-5a99-4a3e-bd51-e924982e5958-targetpath.txt

---
+++ GameAI/WindowsGame2/WindowsGame2/Game1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace WindowsGame2
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        clsSprite mySprite1;
+        clsSprite mySprite2;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 500;
+            graphics.PreferredBackBufferHeight = 300;
+            
+            Content.RootDirectory = "Content";
+        }
+
+        
+        protected override void Initialize()
+        {
+            
+
+            base.Initialize();
+        }
+
+       
+        protected override void LoadContent()
+        {
+            
+            mySprite1 = new clsSprite(Content.Load<Texture2D>("ball"),
+                          new Vector2(0f, 0f), new Vector2(64f, 64f),graphics.PreferredBackBufferWidth,graphics.PreferredBackBufferHeight);
+            mySprite1.velocity = new Vector2(1, 1);
+            mySprite2 = new clsSprite(Content.Load<Texture2D>("ball"),
+                          new Vector2(218f, 118f), new Vector2(64f, 64f), graphics.PreferredBackBufferWidth,graphics.PreferredBackBufferHeight);
+            mySprite2.velocity = new Vector2(3, -3);
+           
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+     
+        }
+
+        
+        protected override void UnloadContent()
+        {
+            mySprite1.texture.Dispose();
+            mySprite2.texture.Dispose();
+        }
+
+        
+        protected override void Update(GameTime gameTime)
+        {
+          
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+            mySprite1.Move();
+            mySprite2.Move();
+            if (mySprite1.Collides(mySprite2))
+            {
+                Vector2 tempVelocity = mySprite1.velocity;
+                mySprite1.velocity = mySprite2.velocity;
+                mySprite2.velocity = tempVelocity;
+            }
+
+            base.Update(gameTime);
+           
+        }
+
+       
+        protected override void Draw(GameTime gameTime)
+        {
+           
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+           
+
+            spriteBatch.Begin(SpriteBlendMode.AlphaBlend);
+            mySprite1.Draw(spriteBatch);
+            mySprite2.Draw(spriteBatch);
+            spriteBatch.End();
+
+         
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ GameAI/WindowsGame2/WindowsGame2/clsSprite.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsGame2
+{
+    using Microsoft.Xna.Framework.Graphics;   // For Texture2D
+    using Microsoft.Xna.Framework;
+    
+    class clsSprite
+    {
+        
+       
+        public Texture2D texture { get; set; }  // Sprite texture, read-only property
+        private Vector2 screenSize { get; set; } // Screen size
+        public Vector2 velocity { get; set; }
+        public Vector2 position
+        {
+            get ; 
+            set ;
+        }  
+        public Vector2 size { get; set; }      // Sprite size in pixels
+        public clsSprite(Texture2D newTexture, Vector2 newPosition, Vector2 newSize, int ScreenWidth, int ScreenHeight)
+        {
+            texture = newTexture;
+            position = newPosition;
+            size = newSize;
+            screenSize = new Vector2(ScreenWidth, ScreenHeight);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+        public void Move()
+        {
+            // If we'll move out of the screen, invert velocity
+            // Checking right boundary
+            if (position.X + size.X + velocity.X > screenSize.X)
+                velocity = new Vector2(-velocity.X, velocity.Y);
+            // Checking bottom boundary
+            if (position.Y + size.Y + velocity.Y > screenSize.Y)
+                velocity = new Vector2(velocity.X, -velocity.Y);
+            // Checking left boundary
+            if (position.X + velocity.X < 0)
+                velocity = new Vector2(-velocity.X, velocity.Y);
+            // Checking upper  boundary
+            if (position.Y + velocity.Y < 0)
+                velocity = new Vector2(velocity.X, -velocity.Y);
+            // Since we adjusted the velocity, just add it to the current position
+            position += velocity;
+        }
+        public bool Collides(clsSprite otherSprite)
+        {
+            // Check if two sprites collide
+            if (this.position.X + this.size.X > otherSprite.position.X &&
+                this.position.X < otherSprite.position.X + otherSprite.size.X &&
+                this.position.Y + this.size.Y > otherSprite.position.Y &&
+                this.position.Y < otherSprite.position.Y + otherSprite.size.Y)
+                return true;
+            else
+                return false;
+        }
+    }
+}
